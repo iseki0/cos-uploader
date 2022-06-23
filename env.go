c1f1c696ae606eb855d64225d63851fbcf46090b
iseki <admin@iseki.space>
2022-06-23 20:47:47 +0800

    fix: environment

--- env.go
+++ env.go
@@ -4,5 +4,5 @@ import "os"
 
 var CosBucketUrl = os.Getenv("COS_BUCKET_URL")
 var CosServiceUrl = os.Getenv("COS_SERVICE_URL")
-var CosSecretId = os.Getenv("COS_SECRET_KEY")
+var CosSecretId = os.Getenv("COS_SECRET_ID")
 var CosSecretKey = os.Getenv("COS_SECRET_KEY")
