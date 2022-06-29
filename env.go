64b91bbfb9032a8151f29b383d1e63b96c4fa84d
iseki <admin@iseki.space>
2022-06-29 17:28:51 +0800

    feat(version): version v1.0.1

--- env.go
+++ env.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"io"
 	"os"
-	"strings"
+	"regexp"
 )
 
 var (
@@ -17,17 +20,37 @@ func init() {
 	CosServiceUrl = os.Getenv("COS_SERVICE_URL")
 	CosSecretId = os.Getenv("COS_SECRET_ID")
 	CosSecretKey = os.Getenv("COS_SECRET_KEY")
-	s := strings.Split(os.Getenv("COS_STR"), ":")
-	if CosBucketUrl == "" && len(s) > 0 {
-		CosBucketUrl = s[0]
+	pattern := regexp.MustCompile("([^\\s=]+)=(\\S*)")
+	var m [][]string
+	if b := os.Getenv("COS_BASE64"); b != "" {
+		r, e := io.ReadAll(base64.NewDecoder(base64.StdEncoding, bytes.NewReader([]byte(b))))
+		if e != nil {
+			panic("decode COS_BASE64 base64 failed")
+		}
+		m = pattern.FindAllStringSubmatch(string(r), -1)
+	} else if b := os.Getenv("COS_STR"); b != "" {
+		m = pattern.FindAllStringSubmatch(b, -1)
 	}
-	if CosServiceUrl == "" && len(s) > 1 {
-		CosServiceUrl = s[1]
-	}
-	if CosSecretId == "" && len(s) > 2 {
-		CosSecretId = s[2]
-	}
-	if CosSecretKey == "" && len(s) > 3 {
-		CosSecretKey = s[3]
+	for _, it := range m {
+		k := it[1]
+		v := it[2]
+		switch k {
+		case "bucket":
+			if CosBucketUrl == "" {
+				CosBucketUrl = v
+			}
+		case "service":
+			if CosServiceUrl == "" {
+				CosServiceUrl = v
+			}
+		case "sid":
+			if CosSecretId == "" {
+				CosSecretId = v
+			}
+		case "sk":
+			if CosSecretKey == "" {
+				CosSecretKey = v
+			}
+		}
 	}
 }
